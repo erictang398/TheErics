ac7936f56b3c3e3d0cfc6c5ba5270a378e03c18d
Eric Tang <tangeric398@gmail.com>
2022-04-28 00:40:34 -0400

    clicking on arrow

--- scripts/home.js
+++ scripts/home.js
@@ -1,42 +1,71 @@
 function App () {
     return (
         <React.Fragment>
-            <div className="topnav" id="myTopnav">
-                <a href="#" className="active">Home</a>
-                <a href="#">Eric Tang</a>
-                <a href="#">Eric Lin</a>
-                <a href="#">Calculators</a>
-                <a href="#">11111</a>
-                <a href="#">22222</a>
-                <a href="#">33333</a>
-                <a href="#">44444</a>
+            <NavBar />
+            <Background />
+            <DownArrow />
+            <Features />
+        </React.Fragment>
+    )
+}
+
+function Background () {
+    return (
+        <div id = "background">
+            <div id = "title-box">
+                 <h1 id="title">
+                     The Erics
+                 </h1>
             </div>
-            <div id = "background">
-                <div id = "title-box">
-                    <h1 id="title">
-                        The Erics
-                    </h1>
-                </div>
-            </div>
-            <div id = "downarrow">
+        </div>
+    )
+}
+
+class DownArrow extends React.Component {
+    render () {
+        return (
+            <a id = "downarrow" href = "#features">
                 <svg className="arrows">
                     <path className="a1" d="M0 0 L30 32 L60 0"></path>
                     <path className="a2" d="M0 20 L30 52 L60 20"></path>
                     <path className="a3" d="M0 40 L30 72 L60 40"></path>
                 </svg>
-            </div>
+             </a>
+        )
+    }
+}
+
+function Features () {
+    return (
+        <div id = "features">
             <div id="f-title-box">
                 <h1 id = "feature-title">
-                    FEATURES
+                  FEATURES
                 </h1>
             </div>
-            <div id = "features">
+            <div id = "f-box-collector">
                 <div className = "feature-boxes" id = "f-box1"></div>
                 <div className = "feature-boxes" id = "f-box2"></div>
                 <div className = "feature-boxes" id = "f-box3"></div>
             </div>
-        </React.Fragment>
+        </div>
     )
+}
+
+class NavBar extends React.Component {
+    render () {
+        const NAVBAR_ITEMS = ["Home", "Eric Tang", "Eric Lin", "Calculators", "11111"]
+        return (
+            <div className="topnav" id="myTopnav">
+                {NAVBAR_ITEMS.map(item => (
+                    <a href = "#" key = {NAVBAR_ITEMS.indexOf(item)} 
+                    className = {NAVBAR_ITEMS.indexOf(item) === 0 && "active" || ""}>
+                    {item}
+                    </a>
+        ))}
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(
